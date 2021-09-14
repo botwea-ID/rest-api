fe2bdc9bb42162cb589713a76a2734ec0b92fe82
Frangky <81846311+Franky404@users.noreply.github.com>
2021-09-14 08:23:14 +0700

    Update yt.js

--- controllers/yt.js
+++ controllers/yt.js
@@ -1,4 +1,4 @@
-const { ytPlay, ytMp3, ytMp4 } = require("../lib/youtube");
+const { ytPlay, ytMp3, ytMp4,Tiktokdl} = require("../lib/youtube");
 const { cekKey } = require('../database/db');
 
 async function youtubePlay(req, res) {
@@ -73,4 +73,30 @@ async function youtubeMp4(req, res) {
     });
 }
 
-module.exports = { youtubePlay, youtubeMp3, youtubeMp4 };
+async function TiktokDl(req, res) {
+    const url = req.query.url;
+    const apikey = req.query.apikey;
+    if (url === undefined || apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter url & apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    Tiktokdl(url).then(result => {
+        res.status(200).send({
+            status: 200, 
+            result: result
+        });
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+}
+
+module.exports = { youtubePlay, youtubeMp3, youtubeMp4 ,TiktokDl};
