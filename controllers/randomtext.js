93c2c27fc3846cd704a53270b251690767247435
Frangky <81846311+Franky404@users.noreply.github.com>
2021-09-18 15:03:55 +0700

    Update randomtext.js

--- controllers/randomtext.js
+++ controllers/randomtext.js
@@ -124,4 +124,23 @@ async function motivasi(req, res) {
     });
 }
 
-module.exports = {cakLontong, quotes, bijak, fakta, ptl, motivasi };
+async function darkjoke(req, res) {
+    const apikey = req.query.apikey;
+    if (apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    readFileTxt('./lib/data/darkjoke.txt').then(result => {
+        res.status(200).send({status: 200, result: result});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({status: 500, message: 'Internal Server Error'});
+    });
+}
+
+module.exports = {cakLontong, quotes, bijak, fakta, ptl, motivasi ,darkjoke};
